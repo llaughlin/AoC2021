774479cfef240a3fac7fa1db009dc79ae3e97028
Logan Laughlin <loganlaughlin@gmail.com>
2021-12-01 09:51:56 -0600

    Day1

---
+++ Days/Day1/Day1.fs
@@ -0,0 +1,37 @@
+module Day1
+
+open System
+open System.IO
+open Xunit
+
+[<Fact>]
+let ``Step 1`` () =
+    let input = File.ReadAllLines("Day1/Day1.input.txt")
+
+    let result =
+        input
+        |> Seq.map Convert.ToInt32
+        |> Seq.pairwise
+        |> Seq.map (fun (a, b) -> b > a)
+        |> Seq.filter id
+        |> Seq.length
+
+    Assert.Equal(0, result)
+
+
+[<Fact>]
+let ``Step 2`` () =
+    let input = File.ReadAllLines("Day1/Day1.input.txt")
+
+    let result =
+        input
+        |> Seq.map Convert.ToInt32
+        |> Seq.windowed 3
+        |> Seq.map (fun win -> (win.[0], win.[1], win.[2]))
+        |> Seq.map (fun (a, b, c) -> a + b + c)
+        |> Seq.pairwise
+        |> Seq.map (fun (a, b) -> b > a)
+        |> Seq.filter id
+        |> Seq.length
+
+    Assert.Equal(0, result)
